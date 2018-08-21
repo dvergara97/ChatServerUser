4898f9e373fbcf925c9c1c87049c03d6672c7f5c
roxas <roxas@MSI>
2018-08-20 20:12:12 -0700

    Commiting so Aaron can try

---
+++ src/DefaultCheck.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ * Author: CHANGE TO AARON
+ * 
+ */
+
+
+public class DefaultCheck {
+
+	public static void main(String[] args) {
+		//Change to hello my name is AARINA TREVINA
+		System.out.println("Hello world!");
+	}
+	
+}
